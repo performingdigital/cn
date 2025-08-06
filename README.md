0443960dfc8c1c038df0a420b03f80fb56ec8f46
Giorgio Pogliani <giorgiopogliani94@gmail.com>
2025-08-06 16:24:04 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Cn php
+
+Simple tailwind merge wrapper for php that converts conditional classes into a single string.
+
+It's using `gehrisandro/tailwind-merge-php` to merge tailwind classes.
+
+```php
+use Performing\Cn\Cn;
+
+$isDark = true;
+Cn::merge('p-1 bg-red-500', ['text-white bg-blue-500' => $isDark]); // p-1 text-white bg-blue-500
+
+$isDark = false;
+Cn::merge('p-1 bg-red-500', ['text-white bg-blue-500' => $isDark]); // p-1 bg-red-500
+```
---
+++ src/Cn.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Performing\Cn;
+
+use TailwindMerge\TailwindMerge;
+
+class Cn
+{
+    public static function cn(...$inputs)
+    {
+        $inputs = array_map(function ($input) {
+            if (is_array($input)) {
+                return implode(' ', array_keys(array_filter($input)));
+            } elseif (is_string($input)) {
+                return trim($input);
+            }
+        }, $inputs);
+
+        return TailwindMerge::instance()->merge(...$inputs);
+    }
+}
